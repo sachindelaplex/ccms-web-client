bd899dce64152bc99f8b150465f0199011039a05
Mahendra Nikhade <mnikhade@catapulthq.com>
2019-08-21 17:13:34 +0530

    new files

---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import './Menu.css';
+import {Link} from 'react-router-dom';
+import { Navbar, Button, Collapse } from 'bootstrap-4-react';
+
+
+class Menu extends Component{
+
+    render(){
+
+        const navbars = {
+            textDecoration: 'none',
+            color: 'darkgray',
+            marginLeft: '20px'
+        }
+          
+        return(
+            <div>
+                <React.Fragment>
+                    <Navbar expand="lg" dark bg="dark" mb="3">
+                    {/* <Navbar.Brand href="#">Welcome to the dashboard {this.props.name} !</Navbar.Brand> */}
+                    <Navbar.Toggler target="#navbarColor1" />
+                    <Collapse navbar id="navbarColor1">
+                        <Navbar.Nav mr="auto">
+                            <Link style={navbars}  to="/dashboard">Dashboard</Link>
+                            <Link style={navbars}  to="/details">Add Details</Link>
+                        </Navbar.Nav>
+                        <Button onClick={this.props.logout} outline info my="2 sm-0">Logout</Button>
+                    </Collapse>
+                    </Navbar>
+                </React.Fragment>
+            </div>
+        )
+    }
+
+}
+export default Menu;
