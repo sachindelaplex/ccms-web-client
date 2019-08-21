1cc38eb8c6444e1283730806ba0a8e6121553d6f
Neha Chavan <nchavan@delaplex.in>
2019-08-21 20:01:05 +0530

    details api

--- src/components/details/details.js
+++ src/components/details/details.js
@@ -3,14 +3,22 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './details.css';
 import axios from 'axios';
 import Select from 'react-select';
+import Menu from '../Menu/Menu';
+// import { Container, Row, Col, Button  } from 'bootstrap-4-react';
 
 function Details(props){   
+var policestationList = [];
+var courtList = [];
+var judgeList = [];
 
     useEffect(() => {
         axios.get('/court/getRegistation')
         .then(function(response) {
           console.log('response ',response.data);
-            
+          policestationList = response.data.policestation
+          console.log('element ',policestationList);
+          courtList = response.data.court;
+          judgeList = response.data.judge; 
         }).catch(function (error) {
             console.log('error ',error);
         });    
@@ -19,17 +27,17 @@ function Details(props){
 
     console.log('details page');
     const [inputs, setInputs] = useState({
-        CCTNS : '',
-        FIR : '',
-        RCC : '',
+        cctns_no : '',
+        fir_no : '',
+        cc_rcc_no : '',
         regDate : '',
         regTime : '',
-        complaint : '',
+        complaints : '',
         accused : '',
-        hearingDate : '',
-        pairaniName : '',
-        ppName : '',
-        ioName : ''
+        hearing_date : '',
+        pairani : '',
+        pp : '',
+        io : ''
     });
     
     const [policeStation, setPoliceStation] = useState(null);
@@ -38,9 +46,9 @@ function Details(props){
     const [bailStatus, setBailStatus] = useState(null);
     const [forensic, setForensic] = useState(null);
     const [ps, setPS] = useState(null);
-    const [ca, setCA] = useState(null);
-    const [dna, setDNA] = useState(null);
-    const [handwritingReport, setHandwritingReport] = useState(null);
+    const [ca_report, setCA] = useState(null);
+    const [dna_report, setDNA] = useState(null);
+    const [handwriting_report, setHandwritingReport] = useState(null);
 
     const options = [
         { value: 'Yes', label: 'Yes' },
@@ -57,29 +65,41 @@ function Details(props){
       }
 
       const submit = () => {
+        // setInputs({...inputs, ["policestation"]: policeStation.name});
+        // setInputs({...inputs, ["court"]: court.name});
+        // setInputs({...inputs, ["judge"]: judge.name});
+
+        // setInputs({...inputs, ["bail"]: bailStatus.value});
+        // setInputs({...inputs, ["forensic"]: forensic.value});
+        // setInputs({...inputs, ["ca_report"]: ca_report.value});
+        // setInputs({...inputs, ["dna_report"]: dna_report.value});
+        // setInputs({...inputs, ["handwriting_report"]: handwriting_report.value});
+        // setInputs({...inputs, ["judge"]: judge.name});
+        
 
       }
 
      return (
         <React.Fragment>
+            <Menu/>
             <form class="form">
                 <div>
                 <label class="d-flex">Case Basic Info :</label>
                 <div class="row mr5">
                 <div class="w-50 pdr-10">
                 <label class="col-5">CCTNS No : </label>
-                <input class="col-7"type="text" name="CCTNS" value={inputs.CCTNS} onChange={handleInputChange}/>
+                <input class="col-7"type="text" name="cctns_no" value={inputs.cctns_no} onChange={handleInputChange}/>
                 </div>
                 <div class="w-50">
                 <label class="col-5">FIR No : </label>
-                <input class="col-7" type="text" name="FIR" value={inputs.FIR} onChange={handleInputChange}/>            
+                <input class="col-7" type="text" name="fir_no" value={inputs.fir_no} onChange={handleInputChange}/>            
                 </div>
                 </div>
 
                 <div class="row mr5">
                 <div class="w-50 pdr-10">
                 <label class="col-5">CC/RCC No : </label>
-                <input class="col-7" type="text" name="RCC" value={inputs.RCC} onChange={handleInputChange}/>
+                <input class="col-7" type="text" name="cc_rcc_no" value={inputs.cc_rcc_no} onChange={handleInputChange}/>
                 </div>
                 <div class="w-50">
                 <label class="col-5">Date of Registration : </label>
@@ -95,7 +115,7 @@ function Details(props){
                 <div class="w-50 row mr-0">
                 <label class="col-5">Police Station :  </label>
                 <div class="col-7 pd-0">
-                    <Select  name="policeStation" value={policeStation} onChange={e => setPoliceStation(e)} options={options}/>
+                    <Select  name="policeStation" value={policeStation} onChange={e => setPoliceStation(e)} options={policestationList.name}/>
                 </div>
 
                 {/* <input class="col-7" type="text" name="policeStation" value={inputs.policeStation} onChange={handleInputChange}/>             */}
@@ -134,7 +154,7 @@ function Details(props){
                 <div class="row mr5">
                 <div class="w-50">
                     <label class="col-5 pdl-0">Complaint</label>
-                    <textarea class="col-7 mrl-5" name="complaint" value={inputs.complaint} onChange={handleInputChange} />
+                    <textarea class="col-7 mrl-5" name="complaints" value={inputs.complaints} onChange={handleInputChange} />
                 </div>
 
                 <div class="w-50">
@@ -146,7 +166,7 @@ function Details(props){
                 <div class="row mr5">
                 <div class="w-50">
                     <label class="col-5 pdl-0">Hearing Date</label>
-                    <input type="date" class="col-7 mrl-5" name="hearingDate" value={inputs.hearingDate} onChange={handleInputChange}/>
+                    <input type="date" class="col-7 mrl-5" name="hearing_date" value={inputs.hearing_date} onChange={handleInputChange}/>
                 </div>
 
                 <div class="w-50 row mr-0">
@@ -182,14 +202,14 @@ function Details(props){
                 <div class="w-50 row mr-0">
                     <label class="col-5">CA Report</label>
                     <div class="col-7 pd-0 mrl-5">
-                    <Select  name="CA" value={ca} onChange={e => setCA(e)} options={options}/>
+                    <Select  name="ca_report" value={ca_report} onChange={e => setCA(e)} options={options}/>
                     </div>
                 </div>
 
                 <div class="w-50 row mr-0">
                     <label class="col-5">DNA Report</label>
                     <div class="col-7 pd-0">
-                    <Select  name="DNA" value={dna} onChange={e => setDNA(e)} options={options}/>
+                    <Select  name="dna_report" value={dna_report} onChange={e => setDNA(e)} options={options}/>
                     </div>
                 </div>
                 </div>
@@ -198,7 +218,7 @@ function Details(props){
                 <div class="w-50 row mr-0">
                     <label class="col-5">Handwriting Report</label>
                     <div class="col-7 pd-0 mrl-5">
-                    <Select  name="handwritingReport" value={handwritingReport} onChange={e => setHandwritingReport(e)} options={options}/>
+                    <Select  name="handwriting_report" value={handwriting_report} onChange={e => setHandwritingReport(e)} options={options}/>
                     </div>
                 </div>
                 </div>
@@ -209,18 +229,18 @@ function Details(props){
                 <div class="row mr5">
                 <div class="w-50 pdr-10">
                 <label class="col-5">Pairani Name : </label>
-                <input class="col-7"type="text" name="pairaniName" value={inputs.pairaniName} onChange={handleInputChange}/>
+                <input class="col-7"type="text" name="pairani" value={inputs.pairani} onChange={handleInputChange}/>
                 </div>
                 <div class="w-50">
                 <label class="col-5">PP Name : </label>
-                <input class="col-7" type="text" name="ppName" value={inputs.ppName} onChange={handleInputChange}/>            
+                <input class="col-7" type="text" name="pp" value={inputs.pp} onChange={handleInputChange}/>            
                 </div>
                 </div>
 
                 <div class="row mr5">
                 <div class="w-50 pdr-10">
                 <label class="col-5">IO Name : </label>
-                <input class="col-7"type="text" name="ioName" value={inputs.ioName} onChange={handleInputChange}/>
+                <input class="col-7"type="text" name="io" value={inputs.io} onChange={handleInputChange}/>
                 </div>
                 </div>
 
